ba2b50265ff1051a05c5c6ebc06413fa8d1d35ab
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-17 06:54:17 +0530

    test

---
+++ test/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {createBrowserRouter} from 'react-router-dom'
+function Navbar() {
+    const router = createBrowserRouter([
+        {},
+        {},
+        {},
+    ])
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
