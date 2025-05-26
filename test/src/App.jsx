e1f71949c6c3052d75860163dd9ac958e1d39d87
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-27 01:21:51 +0530

    Router

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,27 +1,18 @@
 import React from 'react'
-import axios from 'axios'
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home } from './Pages/Home'
+import { About } from './Pages/About'
+import { Contact } from './Pages/Contact'
 const App = () => {
-  const [data, setData] = useState([])
-  const getData = async()=>{
-    const response = await axios.get('https://picsum.photos/v2/list')
-    setData(response.data)
-    console.log(response.data);
-  }
   return (
     <div>
-      <button onClick={getData} className='rounded bg-green-500 text-white'>Get Data</button>
-      {data.map((e, idx)=>{
-        return(
-          <>
-          <div key={idx}>
-            <h1>Name - {e.author}</h1>
-            <img src={e.download_url} alt="Photo" />
-          </div>
-          </>
-        )
-      })}
+      <Routes>
+        <Route path='/'element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
     </div>
   )
 }
+
 export default App
