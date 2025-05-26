0d9e2590be550b9ce7b72d4160c0fb1e3c84a97d
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-27 00:09:59 +0530

    Axios

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,18 +1,27 @@
-import React, { useState } from 'react'
-import { Navbar } from './Components/Navbar'
-import { Display } from './Components/Display'
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 const App = () => {
-  const [user, setUser] = useState('Sahil')
+  const [data, setData] = useState([])
+  const getData = async()=>{
+    const response = await axios.get('https://picsum.photos/v2/list')
+    setData(response.data)
+    console.log(response.data);
+  }
   return (
-    <>
-      <Display/>
-      <button className='btn' onClick={()=>{
-        setUser("Sk")
-      }} onDoubleClick={()=>{
-        setUser("Sahil")
-      }}>Click</button>
-    </>
+    <div>
+      <button onClick={getData} className='rounded bg-green-500 text-white'>Get Data</button>
+      {data.map((e, idx)=>{
+        return(
+          <>
+          <div key={idx}>
+            <h1>Name - {e.author}</h1>
+            <img src={e.download_url} alt="Photo" />
+          </div>
+          </>
+        )
+      })}
+    </div>
   )
 }
-
 export default App
